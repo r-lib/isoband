9c8f8abac9d7037ab135fc9a847cd439e3aa937a
Claus Wilke <wilke@austin.utexas.edu>
2019-01-19 00:11:58 -0600

    more tolerant tests

--- tests/testthat/test-isolines.R
+++ tests/testthat/test-isolines.R
@@ -5,16 +5,16 @@ test_that("line segments get merged", {
   z <- matrix(c(0, 0, 1,
                 1, 1, 1), ncol = 3, nrow = 2, byrow = TRUE)
   out <- isolines(x = 1:3, y = 2:1, z, levels = 0.5)
-  expect_equal(out[[1]]$x, c(1, 2, 2.5))
-  expect_equal(out[[1]]$y, c(1.5, 1.5, 2.0))
+  expect_setequal(out[[1]]$x, c(1, 2, 2.5))
+  expect_setequal(out[[1]]$y, c(1.5, 1.5, 2.0))
   expect_equal(out[[1]]$id, rep(1, 3))
 
   # two unconnected line segments don't get merged
   z <- matrix(c(0, 1, 0,
                 0, 1, 1), ncol = 3, nrow = 2, byrow = TRUE)
   out <- isolines(x = 1:3, y = 2:1, z, levels = 0.5)
-  expect_equal(out[[1]]$x, c(2.5, 3.0, 1.5, 1.5))
-  expect_equal(out[[1]]$y, c(2.0, 1.5, 2.0, 1.0))
+  expect_setequal(out[[1]]$x, c(2.5, 3.0, 1.5, 1.5))
+  expect_setequal(out[[1]]$y, c(2.0, 1.5, 2.0, 1.0))
   expect_equal(out[[1]]$id, rep(1:2, each = 2))
 
   # two separate lines get merged in second row
@@ -22,8 +22,8 @@ test_that("line segments get merged", {
                 0, 1, 0,
                 0, 0, 0), ncol = 3, nrow = 3, byrow = TRUE)
   out <- isolines(x = 1:3, y = 3:1, z, levels = 0.5)
-  expect_equal(out[[1]]$x, c(2.5, 2.5, 2.0, 1.5, 1.5))
-  expect_equal(out[[1]]$y, c(3.0, 2.0, 1.5, 2.0, 3.0))
+  expect_setequal(out[[1]]$x, c(2.5, 2.5, 2.0, 1.5, 1.5))
+  expect_setequal(out[[1]]$y, c(3.0, 2.0, 1.5, 2.0, 3.0))
   expect_equal(out[[1]]$id, rep(1, 5))
 
   # circle gets closed
@@ -31,8 +31,11 @@ test_that("line segments get merged", {
                 0, 1, 0,
                 0, 0, 0), ncol = 3, nrow = 3, byrow = TRUE)
   out <- isolines(x = 1:3, y = 3:1, z, levels = 0.5)
-  expect_equal(out[[1]]$x, c(2.5, 2.0, 1.5, 2.0, 2.5))
-  expect_equal(out[[1]]$y, c(2.0, 2.5, 2.0, 1.5, 2.0))
+  # circle is closed
+  expect_equal(out[[1]]$x[1], out[[1]]$x[5])
+  expect_equal(out[[1]]$y[1], out[[1]]$y[5])
+  # coords are correct
+  expect_setequal(out[[1]]$x, c(2.5, 2.0, 1.5, 2.0, 2.5))
+  expect_setequal(out[[1]]$y, c(2.0, 2.5, 2.0, 1.5, 2.0))
   expect_equal(out[[1]]$id, rep(1, 5))
-
 })
