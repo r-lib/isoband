44dfc613f473527fb18b251ca27962817884db05
Claus Wilke <wilke@austin.utexas.edu>
2019-02-02 22:52:08 -0600

    new tests

--- src/labels.cpp
+++ src/labels.cpp
@@ -67,6 +67,117 @@ point entry_intersection(const point &p1, const point &p2) {
   return point(xint, yint);
 }
 
+
+// calculates the two intersection points ip1 and ip2 (if they exist) of a
+// line segment from p1 to p2 and the unit box, assuming both p1 and p2
+// are outside the box. if the assumption isn't true, results are not reliable.
+// returns false if no intersection exists.
+bool double_intersection(const point &p1, const point &p2, point &ip1, point &ip2) {
+  double dx = p2.x - p1.x;
+  double dy = p2.y - p1.y;
+
+  if (dx == 0) {
+    if (dy == 0) return false; // degenerate case
+
+    // vertical line
+    // trivial cases have been exlcuded by calling function, therefore this is easy
+    ip1.x = p1.x;
+    ip2.x = p1.x;
+    if (p1.y >= 1) {
+      ip1.y = 1;
+      ip2.y = 0;
+    } else {
+      ip1.y = 0;
+      ip2.y = 1;
+    }
+    return true;
+  } else if (dy == 0) {
+    // horizontal line
+    // trivial cases have been exlcuded by calling function, therefore this is easy
+    ip1.y = p1.y;
+    ip2.y = p1.y;
+    if (p1.x >= 1) {
+      ip1.x = 1;
+      ip2.x = 0;
+    } else {
+      ip1.x = 0;
+      ip2.x = 1;
+    }
+    return true;
+  } else {
+    // in the general case, we need to calculate the intersection points with all four edges
+    // we start at the top and go around clockwise, top, right, bottom, left
+    double t1 = (1 - p1.y)/dy;
+    double x1 = p1.x + t1*dx;
+    double t2 = (1 - p1.x)/dx;
+    double y2 = p1.y + t2*dy;
+    double t3 = -1*p1.y/dy;
+    double x3 = p1.x + t3*dx;
+    double t4 = -1*p1.x/dx;
+    double y4 = p1.y + t4*dy;
+
+    int score = 8*(x1 > 0 && x1 <= 1) + 4*(y2 >= 0 && y2 < 1) + 2*(x3 >= 0 && x3 < 1) + (y4 > 0 && y4 <= 1);
+    switch(score) {
+    case 5: // 0101  line through left and right
+      if (t4 < t2) {
+        ip1 = point(0, y4);
+        ip2 = point(1, y2);
+      } else {
+        ip1 = point(1, y2);
+        ip2 = point(0, y4);
+      }
+      return true;
+    case 10: // 1010  line through bottom and top
+      if (t3 < t1) {
+        ip1 = point(x3, 0);
+        ip2 = point(x1, 1);
+      } else {
+        ip1 = point(x1, 1);
+        ip2 = point(x3, 0);
+      }
+      return true;
+    case 12: // 1100  line through top right
+      if (t2 < t1) {
+        ip1 = point(1, y2);
+        ip2 = point(x1, 1);
+      } else {
+        ip1 = point(x1, 1);
+        ip2 = point(1, y2);
+      }
+      return true;
+    case 6: // 0110  line through bottom right
+      if (t3 < t2) {
+        ip1 = point(x3, 0);
+        ip2 = point(1, y2);
+      } else {
+        ip1 = point(1, y2);
+        ip2 = point(x3, 0);
+      }
+      return true;
+    case 3: // 0011  line through bottom left
+      if (t4 < t3) {
+        ip1 = point(0, y4);
+        ip2 = point(x3, 0);
+      } else {
+        ip1 = point(x3, 0);
+        ip2 = point(0, y4);
+      }
+      return true;
+    case 9: // 1001  line through top left
+      if (t4 < t1) {
+        ip1 = point(0, y4);
+        ip2 = point(x1, 1);
+      } else {
+        ip1 = point(x1, 1);
+        ip2 = point(0, y4);
+      }
+      return true;
+    default:
+      return false;
+    }
+  }
+}
+
 segment_crop_type crop_to_unit_box(const point &p1, const point &p2, point &crop1, point &crop2) {
   // trivial case 1: line segment trivially outside box
   if ((p1.x <= 0 && p2.x <= 0) || (p1.x >= 1 && p2.x >= 1) ||
@@ -94,8 +205,10 @@ segment_crop_type crop_to_unit_box(const point &p1, const point &p2, point &crop
     return at_end;
   }
 
-
-  return in_middle;
+  // final case is double intersection in middle or no intersection at all
+  bool crop = double_intersection(p1, p2, crop1, crop2);
+  if (crop) return in_middle;
+  return none;
 }
 
 // [[Rcpp::export]]
@@ -120,17 +233,15 @@ void test(const NumericVector &p1, const NumericVector &p2) {
   case at_end:
     cout << "crop result: at end " << crop1 << endl;
     break;
+  case in_middle:
+    cout << "crop result: in middle " << crop1 << " " << crop2 << endl;
+    break;
   default:
     cout << "crop result: not implemented" << endl;
   }
-
 }
 
 
 /*** R
-test(c(.2, .3), c(.4, .5))
-test(c(-.2, .3), c(.4, -.5))
-test(c(-.2, -.3), c(-.4, -.5))
-test(c(.2, .3), c(1.5, .5))
-test(c(.5, .5), c(1.2, 1.2))
+test(c(-.3, .5), c(.5, 1.3))
 */
--- src/test-labels.cpp
+++ src/test-labels.cpp
@@ -10,6 +10,53 @@ bool near_equal(double x1, double x2) {
 }
 
 context("Crop to unit box") {
+  test_that("Both points inside") {
+    point crop1, crop2;
+    segment_crop_type result;
+
+    result = crop_to_unit_box(point(.2, .3), point(.7, .6), crop1, crop2);
+    expect_true(result == complete);
+    result = crop_to_unit_box(point(.7, .6), point(.2, .3), crop1, crop2);
+    expect_true(result == complete);
+  }
+
+  test_that("Both points trivially outside") {
+    point crop1, crop2;
+    segment_crop_type result;
+
+    // to the left
+    result = crop_to_unit_box(point(-.2, .3), point(-.5, 1.6), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(-.2, .3), point(-.5, .6), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(-.5, 1.6), point(-.2, -.3), crop1, crop2);
+    expect_true(result == none);
+
+    // to the right
+    result = crop_to_unit_box(point(1.2, .3), point(1.5, 1.6), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(1.2, .3), point(1.5, .6), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(1.5, 1.6), point(1.2, -.3), crop1, crop2);
+    expect_true(result == none);
+
+    // above
+    result = crop_to_unit_box(point(.3, 1.2), point(1.6, 1.5), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(.3, 1.2), point(.6, 1.5), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(1.6, 1.5), point(-.3, 1.2), crop1, crop2);
+    expect_true(result == none);
+
+    // below
+    result = crop_to_unit_box(point(.3, -.2), point(1.6, -.5), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(.3, -.2), point(.6, -.5), crop1, crop2);
+    expect_true(result == none);
+    result = crop_to_unit_box(point(1.6, -.5), point(-.3, -.2), crop1, crop2);
+    expect_true(result == none);
+  }
+
   test_that("One point inside") {
     point crop1, crop2;
     segment_crop_type result;
@@ -107,5 +154,22 @@ context("Crop to unit box") {
     expect_true(result == at_beginning);
     expect_true(near_equal(crop1.x, .125));
     expect_true(near_equal(crop1.y, 0));
+
+    // left
+    result = crop_to_unit_box(point(-.4, .5), point(.2, .2), crop1, crop2);
+    expect_true(result == at_end);
+    expect_true(near_equal(crop1.x, 0));
+    expect_true(near_equal(crop1.y, .3));
+
+    // top left
+    result = crop_to_unit_box(point(.2, .8), point(-.1, 1.4), crop1, crop2);
+    expect_true(result == at_beginning);
+    expect_true(near_equal(crop1.x, .1));
+    expect_true(near_equal(crop1.y, 1));
+
+    result = crop_to_unit_box(point(.2, .8), point(-.4, 1.1), crop1, crop2);
+    expect_true(result == at_beginning);
+    expect_true(near_equal(crop1.x, 0));
+    expect_true(near_equal(crop1.y, .9));
   }
 }
