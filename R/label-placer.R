f809db93c30188fb58c885082ed2279713754250
Claus Wilke <wilke@austin.utexas.edu>
2019-03-30 21:34:50 -0500

    forgot file

--- R/label-placer.R
+++ R/label-placer.R
@@ -1,67 +1,52 @@
-# Calculate the label position for one set of isolines (one level). Used by
-# `isolines_grob()` to place labels.
-#
-# @param line_data line segments, specified as list of x, y, id
-# @param break_id character vector specifying the break identifier
-# @param label character vector specifying the label that will be printed
-#   instead of the break identifier
-# @param index index into original list of all isolines
-# @param break_index index into original list of breaks
-#
-# The parameters `break_id`, `label`, `index`, and `break_index` are provided
-# simply so they can be added to the resulting data frame holding
-# label positions
-place_labels <- function(line_data, break_id, label, index, break_index) {
-  # return empty row if either missing line data or missing label
-  if (length(line_data$x) == 0 || is.na(label)) {
-    return(
-      data.frame(
-        index = integer(0),
-        break_index = integer(0),
-        break_id = character(0), label = character(0),
-        x = numeric(0), y = numeric(0), theta = numeric(0),
-        stringsAsFactors = FALSE
+#' Set up a label placement strategy
+#'
+#' The position and rotation of labels is calculated based on the line
+#' data for individual isolines.
+#' @param placement String consisting of any combination of the letters
+#' "t", "r", "b", "l" indicating the placement of labels at the top,
+#' to the right, at the bottom, to the left of the isoline.
+#' @param rot_adjuster Function that standardizes the rotation angles of the labels.
+#' @param n Size of the point neighborhood over which the rotation angle should be
+#' calculated.
+#' @export
+label_placer_minmax <- function(placement = "tb", rot_adjuster = angle_halfcircle_bottom, n = 2) {
+  force_all(placement, rot_adjuster, n)
+
+  function(line_data) {
+    # find location for labels
+    idx <- na.omit(
+      c(
+        which( # placement "top"
+          isTRUE(grepl("t", placement, fixed = TRUE)) & line_data$y == min(line_data$y)
+        )[1],
+        which( # placement "bottom"
+          isTRUE(grepl("b", placement, fixed = TRUE)) & line_data$y == max(line_data$y)
+        )[1],
+        which( # placement "left"
+          isTRUE(grepl("l", placement, fixed = TRUE)) & line_data$x == min(line_data$x)
+        )[1],
+        which( # placement "right"
+          isTRUE(grepl("r", placement, fixed = TRUE)) & line_data$x == max(line_data$x)
+        )[1]
       )
     )
+
+    out <- data.frame(x = numeric(0), y = numeric(0), theta = numeric(0))
+
+    for (i in seq_along(idx)) {
+      out[i, ] <- minmax_impl(line_data, idx[i], n)
+    }
+
+    # standardize rotation angles for text labels
+    out$theta <- rot_adjuster(out$theta)
+
+    out
   }
-
-  # find location for labels
-  idx <- c(
-    which(line_data$y == min(line_data$y))[1],
-    which(line_data$y == max(line_data$y))[1]
-  )
-
-  # calculate label position
-  pos <- label_positions(line_data, idx)
-
-  # return results
-  data.frame(
-    index = index, # index into original list of all isolines
-    break_index = break_index, # index into original list of breaks
-    break_id = break_id,
-    label = label,
-    x = pos$x, y = pos$y, theta = pos$theta,
-    stringsAsFactors = FALSE
-  )
 }
 
-# Calculate the position and rotation of labels based
-# on the x, y data and the index position(s) in the data.
-# The variable `n` sets the neighborhood size, n = 2 means
-# two points in either direction are used.
-label_positions <- function(data, idx, n = 2) {
-  out <- data.frame(x = numeric(0), y = numeric(0), theta = numeric(0))
-
-  for (i in seq_along(idx)) {
-    out[i, ] <- label_position_impl(data, idx[i], n)
-  }
-
-  out
-}
-
-# function that does all the work for label_positions().
+# function that does all the work for the minmax label placer.
 # requires a single index idx
-label_position_impl <- function(data, idx, n = 2) {
+minmax_impl <- function(data, idx, n) {
   # set of indices belonging to this label
   idx_set <- which(data$id == data$id[idx])
   idx_min <- min(idx_set)
@@ -82,4 +67,53 @@ label_position_impl <- function(data, idx, n = 2) {
   m <- cbind(x - xave, y - yave)
   v <- svd(m)$v
   list(x = xave, y = yave, theta = atan2(v[2], v[1]))
+}
+
+
+#' Standardize label angles
+#'
+#' Functions to standardize rotation angles to specific angle ranges.
+#'
+#' `angle_halfcircle_bottom()` standardizes angles to (-pi/2, pi/2].
+#' `angle_halfcircle_right()` standardizes angles to (0, pi].
+#' `angle_halfcircle_zero()` sets all angles to 0.
+#' `angle_identity()` does not modify any angles.
+#' @param theta Numeric vector of angles in radians.
+#' @export
+angle_halfcircle_bottom <- function(theta) {
+  ifelse(
+    theta <= -pi/2,
+    theta + pi,
+    ifelse(
+      theta > pi/2,
+      theta - pi,
+      theta
+    )
+  )
+}
+
+#' @rdname angle_halfcircle_bottom
+#' @export
+angle_halfcircle_right <- function(theta) {
+  ifelse(
+    theta <= 0,
+    theta + pi,
+    ifelse(
+      theta > pi,
+      theta - pi,
+      theta
+    )
+  )
+}
+
+#' @rdname angle_halfcircle_bottom
+#' @export
+angle_zero <- function(theta) {
+  rep_len(0, length(theta))
+}
+
+#' @rdname angle_halfcircle_bottom
+#' @export
+angle_identity <- function(theta) {
+  theta
 }
