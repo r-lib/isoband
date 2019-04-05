311d1db2df437f2470d876863ddb0914aabd8121
Claus Wilke <wilke@austin.utexas.edu>
2019-04-05 17:09:32 -0500

    make regression tests less strict.

--- tests/testthat/test-clip-lines.R
+++ tests/testthat/test-clip-lines.R
@@ -19,37 +19,37 @@ test_that("basic clipping", {
 
   # clip top right corner
   out <- clip_lines_impl(x, y, id, 1, 1, 1, 1, 0)
-  expect_identical(out$x, c(0.0, 0.0, 0.5, 1.0, 1.0, 0.0))
-  expect_identical(out$y, c(0.0, 1.0, 1.0, 0.5, 0.0, 0.0))
+  expect_equal(out$x, c(0.0, 0.0, 0.5, 1.0, 1.0, 0.0))
+  expect_equal(out$y, c(0.0, 1.0, 1.0, 0.5, 0.0, 0.0))
   expect_identical(out$id, c(rep(1L, 3), rep(2L, 3)))
 
   # clip bottom left corner
   out <- clip_lines_impl(x, y, id, 0, 0, 1, 1, 0)
-  expect_identical(out$x, c(0.0, 0.0, 1.0, 1.0, 0.5))
-  expect_identical(out$y, c(0.5, 1.0, 1.0, 0.0, 0.0))
+  expect_equal(out$x, c(0.0, 0.0, 1.0, 1.0, 0.5))
+  expect_equal(out$y, c(0.5, 1.0, 1.0, 0.0, 0.0))
   expect_identical(out$id, rep(1L, 5))
 
   # clip right half
   out <- clip_lines_impl(x, y, id, 1, .5, 1, 2, 0)
-  expect_identical(out$x, c(0.0, 0.0, 0.5, 0.5, 0.0))
-  expect_identical(out$y, c(0, 1, 1, 0, 0))
+  expect_equal(out$x, c(0.0, 0.0, 0.5, 0.5, 0.0))
+  expect_equal(out$y, c(0, 1, 1, 0, 0))
   expect_identical(out$id, c(rep(1L, 3), 2L, 2L))
 
   # clip left half
   out <- clip_lines_impl(x, y, id, 0, .5, 1, 2, 0)
-  expect_identical(out$x, c(0.5, 1.0, 1.0, 0.5))
-  expect_identical(out$y, c(1, 1, 0, 0))
+  expect_equal(out$x, c(0.5, 1.0, 1.0, 0.5))
+  expect_equal(out$y, c(1, 1, 0, 0))
   expect_identical(out$id, rep(1L, 4))
 
   # clip in middle
   out <- clip_lines_impl(x, y, id, .5, .5, 2, .5, 0)
-  expect_identical(out$x, c(0, 0, 0, 0, 1, 1, 1, 1, 0))
-  expect_identical(out$y, c(0.00, 0.25, 0.75, 1.00, 1.00, 0.75, 0.25, 0.00, 0.00))
+  expect_equal(out$x, c(0, 0, 0, 0, 1, 1, 1, 1, 0))
+  expect_equal(out$y, c(0.00, 0.25, 0.75, 1.00, 1.00, 0.75, 0.25, 0.00, 0.00))
   expect_identical(out$id, c(rep(1L, 2), rep(2L, 4), rep(3L, 3)))
 
   out <- clip_lines_impl(x, y, id, .5, .5, .5, 2, 0)
-  expect_identical(out$x, c(0.00, 0.00, 0.25, 0.75, 1.00, 1.00, 0.75, 0.25, 0.00))
-  expect_identical(out$y, c(0, 1, 1, 1, 1, 0, 0, 0, 0))
+  expect_equal(out$x, c(0.00, 0.00, 0.25, 0.75, 1.00, 1.00, 0.75, 0.25, 0.00))
+  expect_equal(out$y, c(0, 1, 1, 1, 1, 0, 0, 0, 0))
   expect_identical(out$id, c(rep(1L, 3), rep(2L, 4), rep(3L, 2)))
 })
 
@@ -59,13 +59,13 @@ test_that("clip multiple line segments", {
   id <- c(rep(1L, 4), rep(2L, 4))
 
   out <- clip_lines_impl(x, y, id, 0, 0, 1, 3, 0)
-  expect_identical(out$x, c(0.5, 1.0, 1.0, 2.0, 2.0, 3.0, 3.0))
-  expect_identical(out$y, c(1, 1, 0, 2, 3, 3, 2))
+  expect_equal(out$x, c(0.5, 1.0, 1.0, 2.0, 2.0, 3.0, 3.0))
+  expect_equal(out$y, c(1, 1, 0, 2, 3, 3, 2))
   expect_identical(out$id, c(rep(1L, 3), rep(2L, 4)))
 
   out <- clip_lines_impl(x, y, id, 1.5, 1.5, 2, 4, 0)
-  expect_identical(out$x, c(0.0, 0.0, 0.5, 2.5, 3.0, 3.0))
-  expect_identical(out$y, c(0, 1, 1, 3, 3, 2))
+  expect_equal(out$x, c(0.0, 0.0, 0.5, 2.5, 3.0, 3.0))
+  expect_equal(out$y, c(0, 1, 1, 3, 3, 2))
   expect_identical(out$id, c(rep(1L, 3), rep(2L, 3)))
 })
 
@@ -110,8 +110,8 @@ test_that("singletons are carried over or clipped", {
   id <- c(rep(1L, 3), 2L, rep(3L, 4))
 
   out <- clip_lines_impl(x, y, id, 10, 10, 1, 1, 0)
-  expect_identical(out$x, x)
-  expect_identical(out$y, y)
+  expect_equal(out$x, x)
+  expect_equal(out$y, y)
   expect_identical(out$id, id)
 
   out <- clip_lines_impl(x, y, id, 1, 0, .1, .1, 0)
@@ -124,8 +124,8 @@ test_that("singletons are carried over or clipped", {
   id <- c(rep(1L, 3), rep(2L, 4), 3L)
 
   out <- clip_lines_impl(x, y, id, 10, 10, 1, 1, 0)
-  expect_identical(out$x, x)
-  expect_identical(out$y, y)
+  expect_equal(out$x, x)
+  expect_equal(out$y, y)
   expect_identical(out$id, id)
 
   out <- clip_lines_impl(x, y, id, 3, 2, .1, .1, 0)
