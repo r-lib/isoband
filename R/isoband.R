a6027d630e58d311c5d5d28b75e17e8296e23470
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-04-24 10:24:55 +0200

    Fix package docs

--- R/isoband.R
+++ R/isoband.R
@@ -1,9 +1,4 @@
-#' Generate isolines and isobands
-#'
-#' @name isoband
-#' @docType package
 #' @useDynLib isoband, .registration = TRUE
 #' @importFrom utils modifyList
 #' @import grid
-NULL
-
+"_PACKAGE"
